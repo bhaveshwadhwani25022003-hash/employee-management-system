add52e06dbe6d0521bc9a7595e3eab6f6c6d118a
Bhavesh Wadhwani <bhavesh.wadhwani25022003@gmail.com>
2026-05-13 11:41:51 +0530

    feat: add UserRepository, DepartmentRepository, EmployeeRepository

---
+++ employee-management/src/main/java/com/bhavesh/employee_management/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.bhavesh.employee_management.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bhavesh.employee_management.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+  Optional<User> findByUsername(String username);  
+} 
